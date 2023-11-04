19474a96aed0e6b4728b00efaec60f94ea2127f2
Qinastha <qinastha@192.168.1.119>
2023-11-04 18:02:32 +0200

    added readme and updated js

--- b5.js
+++ b5.js
@@ -32,3 +32,23 @@ if (document.querySelector(".dark")) {
 function fun() {
     alert("Nice number bro");
 }
+
+let conf = confirm("You must be over 18y.o. to visit this page");
+let Name = prompt("Please enter your name", "porn_lover");
+let age = prompt("How old are you?", "");
+
+if(age>=18){
+    alert(`Hello ${Name}`) ;
+    let porn_type = prompt ("Which typy of p**n do your prefer the most? Type 'T' for 'Transgender', 'B' for 'Bisexual', 'L' for 'Lesbian, 'G' for 'Gay'", "");
+    if(porn_type=='T') {
+    alert("lmaooo i didnt know you are like that dude, you mum gotta know bout this");
+    }else if(porn_type=='B'){
+    alert("Not too bad, but still watching this makes you an ass!!!");
+    } else if(porn_type=='L') {
+    alert("Hope you know this aint straight and right, i guess none of your parents is proud of yuh");
+    } else if(porn_type=='G') {
+    alert("Jesus, so you didnt know God forbids this, your parents too!!!");
+    } }
+else{
+    alert(`Hi ${Name}. You cant watch this content in your ${age}. Please wait ${18-age} years to visit us!`)
+}
